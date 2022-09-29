7d720f5def364b8cb93e9b432ccea762166946c5
neveennahar <neveennahar67@gmail.com>
2022-09-29 15:46:11 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function App() {
+
+  return (
+    <div className='px-5'>
+      kicho ekta
+
+
+
+
+
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
