ca76e1805e6c59506ba28e4dcfa21656dbd768e1
neveennahar <neveennahar67@gmail.com>
2022-09-29 19:05:11 +0600

    component modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import Activities from './components/Activities';
+import Navbar from './components/Navbar';
+import Questions from './components/Questions';
 
 function App() {
 
   return (
     <div className='px-5'>
-      kicho ekta
+      <Navbar />
+      <div>
+        <Activities />
+        <Questions />
+      </div>
 
 
 
