35f87a0a6ea7a3e8cbd5018bad8e544e62700658
neveennahar <neveennahar67@gmail.com>
2022-09-29 15:51:57 +0600

    navabr added

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='bg-base-100 shadow-md'>
+
+            <div className="navbar mb-5 w-[80%] mx-auto">
+                <a href='/' className=" text-blue-500  normal-case text-xl">Swimming Group</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
